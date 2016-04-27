44faa8cafcad2d9a0e14f7bb0e8203bae33acefd
William Whitacre <wgwhitacre1@cougars.ccis.edu>
2016-04-27 00:42:08 -0500

    Commit 1.0.0, initial commit for elm-lexer.

---
+++ src/Lexer.elm
@@ -0,0 +1,159 @@
+module Lexer
+  ( Class
+  , Lexeme
+  , Lexer
+
+  , class
+  , lexer
+
+  , lex
+
+  , pattern
+  , interpret
+  ) where
+
+{-| An easy to use lexer written using `Bogdanp/elm-combine` that provides a sufficient backbone for use
+with LR(k) parser generators, or as a standalone for simpler higher order pattern matching.
+
+# Types
+@docs Class, Lexeme, Lexer
+
+# Construction
+@docs class, lexer
+
+# Lexing
+@docs lex
+
+# Primitive
+@docs pattern, interpret
+
+-}
+
+import Combine as Cb
+import String
+
+
+{-| A lexeme class, consisting of a regex pattern and a transducer that will interpret' the token
+and spit out an associated lexeme. -}
+type Class lex =
+  Class
+    { regexp : String
+    , interpret' : String -> Result (List String) lex
+    }
+
+
+{-| A lexeme, including the original string token, the interpreted lexeme, the index of
+occurrence, and the length. -}
+type alias Lexeme lex =
+  { token : String
+  , lexeme : lex
+  , index : Int
+  , length : Int
+  }
+
+
+{-| A lexer. -}
+type Lexer lex =
+  Lexer
+    { classes : List (Class lex)
+    , lexemeChoiceParser : Cb.Parser (Lexeme lex)
+    , parser : Cb.Parser (List (Lexeme lex))
+    }
+
+
+{-| Create a new lexeme class from a Javascript style regex and an function to interpret a lexeme
+from the matched token. -}
+class : String -> (String -> Result (List String) lex) -> Class lex
+class regexp interpret' =
+  Class
+    { regexp = regexp
+    , interpret' = interpret'
+    }
+
+
+{-| Get the pattern of a class. -}
+pattern : Class lex -> String
+pattern (Class class) =
+  class.regexp
+
+
+{-| Interpret a token. -}
+interpret : Class lex -> String -> Result (List String) lex
+interpret (Class class) input =
+  class.interpret' input
+
+
+
+
+patternParser_ : Class lex -> Cb.Parser lex
+patternParser_ (Class {regexp, interpret'}) =
+  Cb.regex regexp `Cb.andThen`
+    (\smatch -> case interpret' smatch of
+      Result.Ok lexeme -> Cb.succeed lexeme
+      Result.Err errors -> Cb.fail errors
+    )
+
+
+captureLexeme_ : Cb.Parser lex -> Cb.Context -> (Result (List String) (Lexeme lex), Cb.Context)
+captureLexeme_ lexParser {position, input} =
+  let
+    (result, context') =
+      Cb.parse lexParser input
+
+  in
+    ( Result.map
+        (\lexeme ->
+          { lexeme = lexeme
+          , token = String.left context'.position input
+          , index = position
+          , length = context'.position
+          }
+        ) result
+    , { context'
+      | position =
+          position + context'.position
+      }
+    )
+
+
+lexemeParser_ : Class lex -> Cb.Parser (Lexeme lex)
+lexemeParser_ =
+  patternParser_ >> captureLexeme_ >> Cb.primitive
+
+
+{-| Create a lexer from a list of lexeme classes. -}
+lexer : List (Class lex) -> Lexer lex
+lexer classes =
+  let
+    lexemeChoiceParser =
+      List.map lexemeParser_ classes
+      |> Cb.choice
+
+  in
+    Lexer
+      { classes = classes
+      , lexemeChoiceParser = lexemeChoiceParser
+      , parser = Cb.many lexemeChoiceParser
+      }
+
+
+{-| Lex a string, producing a list of errors (if any) and a list of lexemes. -}
+lex : Lexer lex -> String -> (List String, List (Lexeme lex))
+lex (Lexer {parser}) input =
+  let
+    (result, context) =
+      Cb.parse parser input
+
+    completionErrors =
+      if context.input /= "" then
+        [ "Unrecognized token in input at " ++ toString context.position ++ "."
+        , "Can't lex input: " ++ context.input
+        ]
+      else
+        [ ]
+  in
+    case result of
+      Result.Ok lexemes ->
+        (completionErrors, lexemes)
+      Result.Err errors ->
+        (errors ++ completionErrors, [ ])
